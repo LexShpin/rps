93a71fdc467445e8fc13245d4a2d854c820a2c57
lexshpin <lexshpin@gmail.com>
2023-12-29 16:51:37 -0500

    Logic and simple design are complete

--- script.js
+++ script.js
@@ -2,10 +2,34 @@ const ROCK = 'rock'
 const PAPER = 'paper'
 const SCISSORS = 'scissors'
 
+const PLAYER = 'Player'
+const COMPUTER = 'Computer'
+
+// Player Buttons
+const rockBtn = document.getElementById('rock-btn')
+const paperBtn = document.getElementById('paper-btn')
+const scissorsBtn = document.getElementById('scissors-btn')
+const playerBtns = document.querySelectorAll('.play-btn')
+
+// Computer Buttons
+const computerRockBtn = document.getElementById('computer-rock-btn')
+const computerPaperBtn = document.getElementById('computer-paper-btn')
+const computerScissorsBtn = document.getElementById('computer-scissors-btn')
+
+// Texts
+const results = document.getElementById('results')
+const resultText = document.getElementById('results__text')
+const playerScoreText = document.getElementById('player-score-text')
+const computerScoreText = document.getElementById('computer-score-text')
+
+// Start over btn
+const startOverBtn = document.querySelector('.start-over-btn')
+
 let playerSelection
 
 let playerScore = 0
 let computerScore = 0
+let winner
 
 const getComputerChoice = () => {
     const choices = ['rock', 'paper', 'scissors']
@@ -14,8 +38,6 @@ const getComputerChoice = () => {
 }
 
 const playRound = (playerSelection, computerSelection) => {
-    computerSelection = getComputerChoice()
-
 
     playerSelection = playerSelection.toLowerCase()
 
@@ -47,20 +69,65 @@ const playRound = (playerSelection, computerSelection) => {
 
 const declareWinner = () => {
     if (playerScore == 5) {
+        winner = PLAYER
         console.log('Player WON!')
+        finishGame()
     } else if (computerScore == 5) {
+        winner = COMPUTER
         console.log("Computer WON!")
+        finishGame()
     }
 }
 
-const game = () => {
-    while (playerScore < 5 && computerScore < 5) {
-        playRound()
-        console.log(`Player score: ${playerScore}`)
-        console.log(`Computer score: ${computerScore}`)
-    }
+const updateUI = () => {
+    playerScoreText.textContent = `Player: ${playerScore}`
+    computerScoreText.textContent = `Computer: ${computerScore}`
+}
 
+const finishGame = () => {
+    playerBtns.forEach(btn => {
+       btn.setAttribute('disabled', '')
+    })
+
+    results.style.display = 'flex'
+    resultText.textContent = `${winner} won! Do you want to restart?`
+}
+
+const startGame = () => {
+    playerBtns.forEach(btn => {
+        btn.removeAttribute('disabled', '')
+    })
+
+    playerScore = 0
+    computerScore = 0
+
+    results.style.display = 'none'
+
+    updateUI()
+}
+
+// EVENT LISTENERS
+rockBtn.addEventListener('click', e => {
+    playRound(ROCK, getComputerChoice())
+    updateUI()
     declareWinner()
-}
+})
 
-game()
+paperBtn.addEventListener('click', e => {
+    playRound(PAPER, getComputerChoice())
+    updateUI()
+    declareWinner()
+})
+
+scissorsBtn.addEventListener('click', e => {
+    playRound(SCISSORS, getComputerChoice())
+    updateUI()
+    declareWinner()
+})
+
+startOverBtn.addEventListener('click', e => {
+    console.log(e.target)
+    startGame()
+})
+
+startGame()
